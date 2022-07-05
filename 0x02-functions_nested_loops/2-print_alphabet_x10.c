0059604e03f2ae4a0297f7d8bd331e09190c1195
LinusConradM <linusconradm@gmail.com>
2022-07-05 08:34:18 -0700

    linter

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		char c = 'a';
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
