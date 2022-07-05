23831bb2aa51f1d11cc900d4c64b080cbdd064a3
LinusConradM <linusconradm@gmail.com>
2022-07-05 10:49:08 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,53 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 - prints numbers to 98
- * @n: starting integer
+ * print_times_table - Prints the n times table, starting with 0
  *
- * Description: prints all natural numbers from n - 98
+ * @n: The number to be treated
  *
- * return: void
- *
+ * Return: Number matrix
  */
 
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	if (n < 98)
-	{
-		while (n < 98)
-		{
-			printf("%d, ", n);
-			n++;
-		}
-	}
-	else if (n > 98)
-	{
-		while (n > 98)
-		{
-			printf("%d, ", n);
-			n--;
-		}
-	}
-	printf("98\n");
+	int x, y, z;
 
+	if (n >= 0 && n <= 14)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
