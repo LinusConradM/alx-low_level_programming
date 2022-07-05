a2a4c5ff12aed52c6df9fb8f00a3f4c7d8caa3db
LinusConradM <linusconradm@gmail.com>
2022-07-05 05:03:07 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * main - Prints _putchar, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i =0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+
+	}
+
+return (0);
+}
