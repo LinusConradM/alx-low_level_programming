2166c0e8e4a060c68f9ffec817ab3f702d1fc8e5
LinusConradM <linusconradm@gmail.com>
2022-07-07 10:44:28 -0700

    fixed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,14 @@
  * Return: void
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
-	int num;
-	for (num = 0; num <=9; num++)
-		_putchar((num % 10) + '0');
-	_putchar('\n');
+	char a = 0;
 
+	while (a <= 9)
+	{
+		_putchar(a + '0');
+		a++;
+	}
+	_putchar('\n');
 }
